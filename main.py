cb95eb8179b54adacaf82e90d45ebc37fb5fa94f
bitdeveloppeur <codingbit40@gmail.com>
2025-04-23 10:34:14 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+sys.path.insert(0, os.path.abspath("."))
+
+# Importer le  spider
+from ia_scraper.ia_scraper.spiders.articles import SmartAISpider
+
+def run():
+
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(SmartAISpider )
+    process.start()
+    print(" end scrapy.")
+
+if __name__ == "__main__":
+    run()
+
+
+
